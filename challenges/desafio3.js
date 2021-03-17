37c96a05454ca0f2b768a24d2f432b22a7475e03
Vitor Rodrigues <vitor_rc@outlook.com>
2021-03-17 11:51:48 -0300

    Requisito 6

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,19 +1,19 @@
 db.produtos.updateMany(
   {},
-  { $set: { avaliacao: 0 } }
+  { $set: { avaliacao: 0 } },
 );
 
 db.produtos.updateMany(
   { tags: { $all: ["bovino"] } },
-  { $inc: { avaliacao: 5 } }
+  { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
   { tags: { $all: ["ave"] } },
-  { $inc: { avaliacao: 3 } }
+  { $inc: { avaliacao: 3 } },
 );
 
 db.produtos.find(
   {},
-  { _id: 0, nome: 1, avaliacao: 1 }
+  { _id: 0, nome: 1, avaliacao: 1 },
 );
--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: true } }
+  { $currentDate: { ultimaModificacao: true } },
 );
 
 db.produtos.find(
   { ultimaModificacao: { $exists: true } },
-  { _id: 0, nome: 1 }
+  { _id: 0, nome: 1 },
 );
